a71026d82a8632fc96e1e0cd7aff6d1303ff53ef
chavannikhil802 <chavannikhil802@gmail.com>
2023-05-20 14:34:35 +0530

    Video 2 completed

---
+++ video-2-variables/02_variables.js
@@ -0,0 +1,10 @@
+let a = 67; // a contains integer: 67
+console.log(a); 
+
+a = "nikhil"; // a contains string: harry
+console.log(a);
+
+a = "NIKHIL"; // a contains string : NIKHIL
+console.log(a);
+
+// WE CAN CHANGE THE VARIABLE TYPE AT RUNTIME IN JAVASCRIPT
