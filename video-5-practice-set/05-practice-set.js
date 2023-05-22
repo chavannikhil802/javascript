857b47dd0dc7dca6e4ba461be473903c3398925f
chavannikhil802 <chavannikhil802@gmail.com>
2023-05-22 11:01:13 +0530

    Question 3 answer updated in the file

--- video-5-practice-set/05-practice-set.js
+++ video-5-practice-set/05-practice-set.js
@@ -8,8 +8,13 @@ console.log(a+b);
 console.log(typeof (a+b));
 
 // Question 3 - Create a const object in javascript. Can you change it to hold a number later?
-const a  = {
+const c  = {
     name: "Nikhil",
     section: 1,
     isPrincipal: false
 };
+
+c = 24;
+// You cannot change the value of a constant variable
+
+
