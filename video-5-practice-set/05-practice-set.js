599833934509322c9ae6dabea904ce3aa98826a2
chavannikhil802 <chavannikhil802@gmail.com>
2023-05-22 10:57:58 +0530

    Question 3 and its answer updated in the file

--- video-5-practice-set/05-practice-set.js
+++ video-5-practice-set/05-practice-set.js
@@ -6,3 +6,10 @@ console.log(a+b);
 
 // Question 2 - Use the typeof operator to find the datatype of the variable in question 1.
 console.log(typeof (a+b));
+
+// Question 3 - Create a const object in javascript. Can you change it to hold a number later?
+const a  = {
+    name: "Nikhil",
+    section: 1,
+    isPrincipal: false
+};
