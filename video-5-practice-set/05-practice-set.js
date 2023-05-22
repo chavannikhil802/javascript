940d9cea93263d8673b66ec390bb9344f134b422
chavannikhil802 <chavannikhil802@gmail.com>
2023-05-22 10:54:15 +0530

    Question 2 and its answer updated in the file

--- video-5-practice-set/05-practice-set.js
+++ video-5-practice-set/05-practice-set.js
@@ -3,3 +3,6 @@ let a  = "nikhil";
 let b = 1809;
 console.log(a+b);
 // Here, a and b gets concatenated and the out is a string
+
+// Question 2 - Use the typeof operator to find the datatype of the variable in question 1.
+console.log(typeof (a+b));
