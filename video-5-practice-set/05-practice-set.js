d30019c68b389beb87b9ceed7adfbb3b0a057797
chavannikhil802 <chavannikhil802@gmail.com>
2023-05-22 10:51:19 +0530

    Question 1 and its answer updated in the file

---
+++ video-5-practice-set/05-practice-set.js
@@ -0,0 +1,5 @@
+// Question 1 - Create a variable of type string and try to add a number to it.
+let a  = "nikhil";
+let b = 1809;
+console.log(a+b);
+// Here, a and b gets concatenated and the out is a string
