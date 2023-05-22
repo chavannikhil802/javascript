a7bdd01a5ed361d71b843d36c26a7f2b5972d00a
chavannikhil802 <chavannikhil802@gmail.com>
2023-05-22 14:31:33 +0530

    Updated the video 6 code

---
+++ video-6-expressions/06-expressions.js
@@ -0,0 +1,54 @@
+let a = 45;
+let b = 4;
+console.log("----ARITHMETIC OPERATORS----");
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a % b = ", a%b);
+console.log("a ** b = ", a**b);
+console.log("a++ = ", a++);
+console.log("a = ", a);
+console.log("a-- = ", a--);
+console.log("a = ", a);
+console.log("++a = ", ++a);
+console.log("a = ", a);
+console.log("--a = ", --a);
+console.log("a = ", a);
+
+console.log();
+console.log("----ASSIGNMENT OPERATORS----");
+c = 24;
+console.log("c = ", c);
+console.log("c+=5 =", c+=5);
+console.log("c-=5 =", c-=5);
+console.log("c*=5 =", c*=5);
+console.log("c/=5 =", c/=5);
+console.log("c%=5 =", c%=5);
+console.log("c**=5 =", c**=5);
+
+console.log();
+console.log("----COMPARISON OPERATORS----");
+let d = 24;
+let e = 9;
+console.log("d == e = ",d==e);
+console.log("d != e = ",d!=e);
+console.log("d === e = ",d===e);
+console.log("d !== e = ",d!==e);
+// === and !== compares the value as well as the type of the variables
+console.log("d <= e = ",d<=e);
+console.log("d >= e = ",d>=e);
+console.log("d < e = ",d<e);
+console.log("d > e = ",d>e);
+
+console.log();
+console.log("----LOGICAL OPERATORS----");
+let f = 24;
+let g = 9;
+console.log(f>g && f==24);
+console.log(f>g || f==9);
+console.log(!false);
+console.log(!true);
+
+// Consider the following equation: 7 + 8 = 15
+// Here, 7 and 8 are operands, + is the operator and 15 is the result
